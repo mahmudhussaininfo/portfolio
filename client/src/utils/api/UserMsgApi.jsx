a9da7f05ac632c45886081dac056016b38e47436
Mahmud <77590197+mahmudhussaininfo@users.noreply.github.com>
2023-08-19 09:18:05 +0600

    url updated now

--- client/src/utils/api/UserMsgApi.jsx
+++ client/src/utils/api/UserMsgApi.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
+
+//api url
+const baseUrl = "https://portfolio-8g7y.onrender.com";
+
 //send User mail
 export const sendUserMail = async (data) => {
   try {
-    const response = await axios.post(
-      "http://localhost:5050/api/v1/portfolio",
-      data
-    );
+    const response = await axios.post(`${baseUrl}/api/v1/portfolio`, data);
     return response.data;
   } catch (error) {
     console.log(error.response.data.message);
